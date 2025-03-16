81e75825348d16be932abae9cbc4a89bde20e580
Alejandro Lerma <alermax@gmail.com>
2025-03-15 23:28:09 -0600

    MManejo de imagenes en dominio ivanegoh.com

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  images: {
+    domains: ['ivanegoh.com', 'www.ivanegoh.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
